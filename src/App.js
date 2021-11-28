5cf695a1e143b20da5a43378a15a9cd85e559bb8
Hey OlehPetko! <petkooleg@gmail.com>
2021-11-28 17:51:39 +0000

    innit commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
+import Task from "./Task";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [list, setList] = useState([])
+
+
+    const getList = () => {
+        axios.get('https://nazarov-kanban-server.herokuapp.com/card')
+            .then(res => {
+                console.log(res)
+                setList(res.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    const createCard = async () => {
+      await  axios.post('https://nazarov-kanban-server.herokuapp.com/card', {
+            name: 'Drag and Drop',
+            description: 'I love draw'
+        })
+            .then(res => console.log(res))
+            .catch(error => console.log(error))
+        getList()
+    }
+    const deleteCard =  async (id) => {
+      await axios.delete(`https://nazarov-kanban-server.herokuapp.com/card/${id}`)
+          .then(res => console.log(res))
+          .catch(error => console.log(error))
+        getList()
+    }
+
+
+    return (
+        <div className="App">
+            <button onClick={getList}>getTask</button>
+            <button onClick={createCard}>create new card</button>
+            {list.map(task =>
+                <div>
+                    <Task task={task} deleteCard={deleteCard}/>
+                </div>
+            )}
+
+        </div>
+    );
 }
 
 export default App;
